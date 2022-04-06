be9ab4a207493209628e8c38469aa53dda88264c
AlpakaTitikaka <103045962+AlpakaTitikaka@users.noreply.github.com>
2022-04-06 12:06:39 +0300

    Update TT.py

--- TT.py
+++ TT.py
@@ -1,8 +1,8 @@
-#This file is part of NN-for-forest-safety.
+#This file is part of processing-of-sound-signals-by-a-NN.
 
-#NN-for-forest-safety is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#processing-of-sound-signals-by-a-NN is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-#NN-for-forest-safety is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#processing-of-sound-signals-by-a-NN is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 #You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
 

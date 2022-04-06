3299a70261eea8be5afca67c932a8b23bd7a22d0
AlpakaTitikaka <103045962+AlpakaTitikaka@users.noreply.github.com>
2022-04-06 12:07:21 +0300

    Update util.py

--- util.py
+++ util.py
@@ -1,8 +1,8 @@
-#This file is part of MoiTrudi.
+#This file is part of processing-of-sound-signals-by-a-NN.
 
-#MoiTrudi is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#processing-of-sound-signals-by-a-NN is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-#MoiTrudi is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#processing-of-sound-signals-by-a-NN is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 #You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
 
